4e4028de4f87867649df67c1caa5b81c82bed0e1
Himanshu <Himanshu.Himanshu@gds.ey.com>
2022-09-04 15:08:16 +0530

    Testing

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzDeployementPipeline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
